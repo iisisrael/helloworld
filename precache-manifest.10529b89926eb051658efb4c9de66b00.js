a7d14b96a906d787949683383e1a87b6bc3ca075
Israel J. Carberry <israel@firebrandtech.com>
2020-06-21 12:13:45 -0500

    Updates

---
+++ precache-manifest.10529b89926eb051658efb4c9de66b00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c0891a57980ee3a0f01ea8aa4a28b7",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "9ac3528a5c454f355ae5",
+    "url": "/helloworld/static/css/main.371e6af9.chunk.css"
+  },
+  {
+    "revision": "3140251fdeda5c45de39",
+    "url": "/helloworld/static/js/2.a38d9e86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld/static/js/2.a38d9e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac3528a5c454f355ae5",
+    "url": "/helloworld/static/js/main.e57827a5.chunk.js"
+  },
+  {
+    "revision": "362801de8c9674b15b07",
+    "url": "/helloworld/static/js/runtime-main.ac878301.js"
+  },
+  {
+    "revision": "92e051e53b4baaada39a27ff67e7cccf",
+    "url": "/helloworld/static/media/is_books.92e051e5.jpg"
+  }
+]);
